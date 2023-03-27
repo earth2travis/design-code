f70f4d542d9f532b6d109f0ac058791b89a97c8e
earth2travis <travis.mccutcheon@gmail.com>
2023-03-27 10:44:49 -0500

    Add styles

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,15 +1,45 @@
 import React from "react"
-import { Link } from "gatsby"
+import styled from "styled-components"
+import { themes } from "../styles/ColorStyles"
+import { H1, MediumText } from "../styles/TextStyles"
 
 function HeroSection() {
   return (
-    <>
-      <img src="/images/logos/logo.svg" alt="logo" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-    </>
+    <Wrapper>
+      <ContentWrapper>
+        <TextWrapper>
+          <Title>
+            Design <br />
+            and code React apps
+          </Title>
+          <Description>
+            Don’t skip design. Learn design and code, by building real apps with
+            React and Swift. Complete courses about the best tools.
+          </Description>
+        </TextWrapper>
+      </ContentWrapper>
+    </Wrapper>
   )
 }
 
 export default HeroSection
+
+const Wrapper = styled.div`
+  background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+`
+const ContentWrapper = styled.div`
+  max-width: 1234px;
+  padding: 200px 30px;
+  margin: 0 auto;
+`
+
+const TextWrapper = styled.div`
+  max-width: 360px;
+  display: grid;
+  gap: 30px;
+`
+
+const Title = styled(H1)`
+  color: ${themes.dark.text1};
+`
+const Description = styled(MediumText)``
