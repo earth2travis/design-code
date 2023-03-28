ea276842bcb3e125f5ed5e2bcbadb1a06af00574
earth2travis <travis.mccutcheon@gmail.com>
2023-03-27 19:38:06 -0500

    Add MenuTooltip component

---
+++ src/data/tooltipData.js
@@ -0,0 +1,5 @@
+export const tooltipData = [
+  { title: "Profile", icon: "/images/icons/profile.svg", link: "/profile" },
+  { title: "Settings", icon: "/images/icons/settings.svg", link: "/settings" },
+  { title: "Sign Out", icon: "/images/icons/signout.svg", link: "/signout" },
+]
